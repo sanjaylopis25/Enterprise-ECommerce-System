a9ad87ad31925613cbe0d6b950b7cdbd20bf1acf
sanjaylopis25 <sanjaylopis25@gmail.com>
2026-07-20 20:14:11 +0530

    Create domain entities for User and Role

---
+++ Backend/EnterpriseECommerce/EnterpriseECommerce.Domain/Entities/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseECommerce.Domain.Entities
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+        public ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
---
+++ Backend/EnterpriseECommerce/EnterpriseECommerce.Domain/Entities/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseECommerce.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; }
+
+        public int RoleId { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public int FailedLoginAttempts { get; set; }
+
+        public DateTime? LockoutEnd { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+        public Role Role { get; set; } = null!;
+    }
+}
